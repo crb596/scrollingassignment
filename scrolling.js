1c8b3178fec14ad16cd6c4bc01c0df7bf3575727
cole <57211534+crb596@users.noreply.github.com>
2021-10-11 20:19:28 +0400

    Update scrolling.js

--- scrolling.js
+++ scrolling.js
@@ -1,12 +1,8 @@
 $(document).ready(init);
 
 function init() {
-	/* ========== DRAWING THE PATH AND INITIATING THE PLUGIN ============= */
-
 	$.fn.scrollPath("getPath")
-		// Move to 'start' element
 		.moveTo(100, 190, {name: "start"})
-		// Line to 'description' element
 		.lineTo(200, 410, {
 			name: "loadingPerson",
 			callback: function() {
@@ -14,7 +10,6 @@ function init() {
 				return;
 			}
 		})
-		// Line to 'description' element
 		.lineTo(230, 410, {
 			name: "loadingPerson",
 			callback: function() {
@@ -22,28 +17,11 @@ function init() {
 				return;
 			}
 		})
-		// Line to 'description' element
 		.lineTo(3500, 410, {name: "trees"})
-		// Arc down and line to 'syntax'
+		// Arc for taking off
 		.arc(3500, 10, 400, Math.PI/2, 0, true)
-		// Rotate in place
-		// .rotate(3*Math.PI/2, {
-		// 	name: "rotations-rotated"
-		// })
-		// // Continue upwards to 'source'
 		.lineTo(6600, 10, {
 			name: "end"
 		})
-		// // Small arc downwards
-		// .arc(2250, -700, 150, 0, -Math.PI/2, true)
-
-		// //Line to 'follow'
-		// .lineTo(1350, -850, {
-		// 	name: "follow"
-		// })
-		// // Arc and rotate back to the beginning.
-		// .arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
-
-	// We're done with the path, let's initate the plugin on our wrapper element
 	$(".wrapper").scrollPath({wrapAround: false});
 }
