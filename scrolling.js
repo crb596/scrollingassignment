cde03b0c9f749a4a986b16c714daa7c3086a7f8b
cole <57211534+crb596@users.noreply.github.com>
2021-10-11 20:12:41 +0400

    Update scrolling.js

--- scrolling.js
+++ scrolling.js
@@ -31,7 +31,7 @@ function init() {
 		// 	name: "rotations-rotated"
 		// })
 		// // Continue upwards to 'source'
-		.lineTo(6000, 10, {
+		.lineTo(6500, 10, {
 			name: "end"
 		})
 		// // Small arc downwards
