c3b687ba3194180d9ff7b861b99fda4d86641278
cole <57211534+crb596@users.noreply.github.com>
2021-10-11 18:33:25 +0400

    tree

--- scrolling.js
+++ scrolling.js
@@ -25,9 +25,9 @@ function init() {
 			}
 		})
 		// Line to 'description' element
-		.lineTo(3500, 410, {name: "trees"})
+		.lineTo(4500, 410, {name: "trees"})
 		// Arc down and line to 'syntax'
-		.arc(3500, 200, 200, Math.PI/2, Math.PI/4, false)
+		.arc(4500, 200, 200, Math.PI/2, Math.PI/4, false)
 		// Rotate in place
 		// .rotate(3*Math.PI/2, {
 		// 	name: "rotations-rotated"
